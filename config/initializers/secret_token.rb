e9206b85440d5d70274033a08f47684ee0db4c8c
Makoto Kawai <makoron@za.tnc.ne.jp>
2014-03-27 20:00:31 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IgadonDemoApp::Application.config.secret_key_base = 'dc6b491ba5276a6e13d78a0369ab6cbf80fbaae3ff48b6ad13dbe8b7c28c9a478ac980fa017ab1b11b304783d3f9041bdee04b06f7aa72fd01b848a6e79845ff'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IgadonDemoApp::Application.config.session_store :cookie_store, key: '_igadon_demo_app_session'
